0c46763efafe11353f6294e7a7146e8a832191b1
kelsaylewis <kelsaylewis@gmail.com>
2014-02-19 16:20:12 -0500

    first commit

---
+++ TestProject_withHotkeys/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TestProject_withHotkeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.MaximumSize = new System.Drawing.Size(300, 300);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 100);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 129);
+            this.textBox2.MaximumSize = new System.Drawing.Size(300, 300);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 100);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Search";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CheckforEnter);
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(245, 54);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(547, 334);
+            this.axShockwaveFlash1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 412);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+
+    }
+}
+
---
+++ TestProject_withHotkeys/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotkeys;
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+
+
+namespace TestProject_withHotkeys
+{
+    public partial class Form1 : Form
+    {
+        private Hotkeys.GlobalHotkey ghk;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new Hotkeys.GlobalHotkey(Constants.SHIFT, Keys.F1, this);
+            Debug.WriteLine("I am here in the form init");
+        }
+ 
+        private void HandleHotkey()
+        {
+            WriteLine("Hotkey pressed!");
+        }
+ 
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("I am here in the form load function");
+
+            WriteLine("Trying to register SHIFT+F1");
+            if (ghk.Register())
+                WriteLine("Hotkey registered.");
+            else
+                WriteLine("Hotkey failed to register");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ghk.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+        }
+
+        private void WriteLine(string text)
+        {
+            textBox1.Text += text + Environment.NewLine;
+        }
+
+        // This function grabs the text in the search textbox and searches the database for the video and displays the video
+        private void GetResults()
+        {
+            // Create a new WebClient instance.
+            string remoteUri = "http://smartsign.imtc.gatech.edu/videos?keywords=";
+            remoteUri = remoteUri + textBox2.Text;
+            WebClient myWebClient = new WebClient();
+            // Download home page data.
+            Debug.WriteLine("Downloading " + remoteUri);
+            // Download the Web resource and save it into a data buffer. 
+            byte[] myDataBuffer = myWebClient.DownloadData(remoteUri);
+
+            // Display the downloaded data. 
+            string download = Encoding.ASCII.GetString(myDataBuffer);
+            Debug.WriteLine(download);
+
+            // Process the resulting XML
+            string youTubeID = Regex.Match(download, @"^\[\s*{\s*""id"":\s*""(\w*)"",.*").Groups[1].Value;
+            string youTubeBase = "http://www.youtube.com/v/";
+            axShockwaveFlash1.Movie = youTubeBase + youTubeID;
+        }
+
+        private void CheckforEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                GetResults();
+            }             
+        }
+
+    }
+}
+
